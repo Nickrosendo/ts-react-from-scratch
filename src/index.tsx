110522d8f7b59a93ad7be61e9d79f544005597d0
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2019-11-20 22:31:51 -0300

    initial configuration running with ts, react, webpack and webpack-dev-server

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+ReactDom.render(
+  React.createElement("div", null, "Hello World"),
+  document.getElementById("root")
+);
